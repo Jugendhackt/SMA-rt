8a29915150ff6878d1cd8ef2b13d6abc4e5732c5
Marvin Borner <marvin-borner@live.com>
2019-11-02 22:20:01 +0100

    Added awesomenessy algorithm stuff

--- src/backend/osm.py
+++ src/backend/osm.py
@@ -1,20 +1,29 @@
 import requests
 import json
-import math
+from math import radians, cos, sin, asin, sqrt
+
+def haversine(lon0, lat0, lon1, lat1):
+    lon0, lat0, lon1, lat1 = map(radians, [lon0, lat0, lon1, lat1])
+    dlon = lon1 - lon0 
+    dlat = lat1 - lat0 
+    a = sin(dlat/2)**2 + cos(lat0) * cos(lat1) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371
+    return c * r
 
 range = 2 # km
 lat = 48.4
 lon = 9.98
-lat0 = lat + range * (1 / 119.574)
-lon0 = lon + range * (1 / (111.320 * math.cos(lat)))
-lat1 = lat - range * (1 / 119.574)
-lon1 = lon - range * (1 / (111.320 * math.cos(lat)))
+lat1 = lat + range * (1 / 110.574)
+lon1 = lon + range * (1 / (111.320 * cos(lat)))
+lat0 = lat - range * (1 / 110.574)
+lon0 = lon - range * (1 / (111.320 * cos(lat)))
 
-overpass_url = "https://overpass-api.de/api/interpreter"
+overpass_url = "https://lz4.overpass-api.de/api/interpreter"
 overpass_query = f'''
 [out:json][timeout:25];
 (
-  relation["type"="route"]({lat0},{lon0},{lat1},{lon1});
+  node["shop"="bakery"]({lat0},{lon0},{lat1},{lon1});
 );
 out body;
 '''
@@ -23,7 +32,14 @@ print(overpass_query)
 
 
 response = requests.post(overpass_url, {'data': overpass_query})
+results = response.json()["elements"]
 
-print(response.text)
-#data = response.json()
-print(response.json())
+for result in results:
+    if isinstance(result["lon"], float) and isinstance(result["lat"], float):
+        result["distance"] = haversine(lon, lat, result["lon"], result["lat"])
+    else:
+        del result
+
+results.sort(key=lambda x: x["distance"], reverse=True)
+
+print(results)
