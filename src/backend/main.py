44fbadf7cf5ce08c6ac2bb13ecb781cbc77230e5
Marvin Borner <marvin-borner@live.com>
2019-11-02 12:18:33 +0100

    Added frontend

--- src/backend/main.py
+++ src/backend/main.py
@@ -2,9 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/henlo')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, static_url_path='../frontend/')
