6ca7e966dbe3469e76805aa0e4c8ef39591e266c
Marvin Borner <marvin-borner@live.com>
2019-11-02 21:01:43 +0100

    Awesome super duper lat long algopower!

--- src/backend/osm.py
+++ src/backend/osm.py
@@ -1,19 +1,29 @@
 import requests
 import json
+import math
 
-osm_response = requests.get("https://nominatim.openstreetmap.org/reverse?format=json&lat=48.4&lon=9.98")
+range = 2 # km
+lat = 48.4
+lon = 9.98
+lat0 = lat + range * (1 / 119.574)
+lon0 = lon + range * (1 / (111.320 * math.cos(lat)))
+lat1 = lat - range * (1 / 119.574)
+lon1 = lon - range * (1 / (111.320 * math.cos(lat)))
 
 overpass_url = "https://overpass-api.de/api/interpreter"
-overpass_query = """
+overpass_query = f'''
 [out:json][timeout:25];
-area(""" + str(osm_response.json()["osm_id"]) + """)->.searchArea;
 (
-  node["shop"="bakery"](area.searchArea);
+  relation["type"="route"]({lat0},{lon0},{lat1},{lon1});
 );
 out body;
-"""
+'''
+
+print(overpass_query)
+
 
 response = requests.post(overpass_url, {'data': overpass_query})
 
+print(response.text)
 #data = response.json()
 print(response.json())
